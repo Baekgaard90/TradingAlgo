9048c2e4a019a1f54bb9b4164c81ac74a1d7777b
Baekgaard90 <lars_baekgaard@yahoo.dk>
2017-05-25 14:55:32 +0200

    Testing how versioning works

---
+++ codebase/Analysis.py
@@ -0,0 +1,15 @@
+from yahoo_finance import Share
+from matplotlib import dates
+import matplotlib.pyplot as pyplot
+
+
+DB = Share('DANSKE.CO')
+
+P = [[d['Date'],float(d['Close'])] for d in reversed(DB.get_historical('2016-01-01','2017-01-13'))]
+Px = dates.datestr2num([x[0] for x in P])
+Py = [y[1] for y in P]
+
+print(Px[:10])
+print(Py[:10])
+
+pyplot.plot_date(Px,Py,'-')
