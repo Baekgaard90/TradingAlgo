cba1acf8c435278ee15a2f69298161722a61cf1c
Baekgaard90 <lars_baekgaard@yahoo.dk>
2017-05-25 15:33:25 +0200

    ..

--- codebase/Analysis.py
+++ codebase/Analysis.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as pyplot
 
 DB = Share('DANSKE.CO')
 
-P = [[d['Date'],float(d['Close'])] for d in reversed(DB.get_historical('2016-01-01','2017-01-13'))]
+P = [[d['Date'],float(d['Close'])] for d in reversed(DB.get_historical('2017-01-01','2017-04-30'))]
 Px = dates.datestr2num([x[0] for x in P])
 Py = [y[1] for y in P]
 
